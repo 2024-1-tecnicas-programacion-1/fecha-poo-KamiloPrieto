eba025ecbc99d1dbf3088f68f10901b83ee546a6
Camilo Prieto <juanc.prietom@konradlorenz.edu.co>
2024-04-20 17:22:55 -0500

    Entrega de Ejercicio Clase Fecha (Taller 1 Orientado a objetos)
    Se realizo el respectivo código en la clase Fecha con los atributos y métodos que especificaba la guía. También el código de la clase principal en la que también se siguieron las instrucciones y especificaciones de la guía. Por ultimo se hizo la documentación dentro del código de la complejidad temporal de cada método en ambas clases.

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,85 @@
 package com.mycompany.fecha.poo;
 
+import java.util.HashMap;
+
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
-}
+
+    //Privatizacion de la variables
+    private int dia;//O(1)
+    private int mes;//O(1)
+    private int ano;//O(1)
+
+    /*
+    Constructor
+    Complejidad temporal: O(1) Tiempo constante.
+     */
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;//O(1)
+        this.mes = mes;//O(1)
+        this.ano = ano;//O(1)
+    }//Cierre constructor
+
+    /*
+    Metodo para dar la fecha de manera corta
+    Complejidad temporal: O(1) Tiempo constante.
+     */
+    public String fechaCorta() {
+
+        return dia + "/" + mes + "/" + ano;
+    }//Cierre fechaCorta
+
+    //Metodo para validar la fecha
+    //Comlejidad temporal: O(1) Tiempo constante.
+    public boolean validarFecha() {
+        if (mes < 1 || mes > 12 || dia < 1 || dia > 31 || ano < 0) {
+            return false;
+        }//Cierre if depurador
+        //O(1)
+        if (mes == 2) {
+            if (dia == 29) {
+                if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+                    return true;
+                } else {
+                    return false;
+                }//Cierre if febrero biciesto
+                //O(1)
+            } else {
+                return true;
+            }//Cirre if febrero
+        } else if (mes % 2 == 0 && dia == 31) {
+            return false;
+        } else if (mes % 2 != 0 && dia <= 31) {
+            return true;
+        } else {
+            return true;
+        }//Ciere de if verificacion de fecha
+        //O(1)
+
+    }//Cierre Validar fecha
+
+    //Metodo para pasar el mes a palabra
+    //Complejidad temporal: O(1) Tiempo Constante.
+    public String mesLetra() {
+        HashMap<Integer, String> meses = new HashMap<>();//O(1)
+        meses.put(1, "Enero");
+        meses.put(2, "Febrero");
+        meses.put(3, "Marzo");
+        meses.put(4, "Abril");
+        meses.put(5, "Mayo");
+        meses.put(6, "Junio");
+        meses.put(7, "Junio");
+        meses.put(8, "Agosto");
+        meses.put(9, "Septiembre");
+        meses.put(10, "Octubre");
+        meses.put(11, "Noviembre");
+        meses.put(12, "Diciembre");
+        return meses.get(mes);
+    }//Cierre mesLetra
+
+    //Metodo para dar la fecha larga
+    //Complejidad temporal: O(1) Tiempo constante.
+    public String fechaLarga() {
+        return dia + " de " + mesLetra() + " de " + ano;//O(1)
+    }//Cierre fechaLarga
+
+}//Cierre class
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,30 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
-}
+
+    //Metodo main
+    //Complejidad temporal: O(1) Tiempo Constante.
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Buen dia");
+        System.out.println("Porfavor ingrese el dia, mes y año que desee.(una por una)");//O(1)
+        int dia = sc.nextInt();//O(1)
+        int mes = sc.nextInt();//O(1)
+        int ano = sc.nextInt();//O(1)
+
+        Fecha fechaUsuario = new Fecha(dia, mes, ano);//O(1)
+
+        System.out.println(fechaUsuario.fechaCorta());
+        if (fechaUsuario.validarFecha() == true) {
+            System.out.println("La fecha es valida");//O(1)
+            System.out.println(fechaUsuario.fechaLarga());//O(1)
+        } else {
+            System.out.println("La fecha no es valida");//O(1)
+        }//Cierre if de validacion
+        //O(1)
+
+    }//Cierre main
+}//Cierre class Principal
